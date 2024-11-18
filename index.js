8526f797ca1ef1a406283aee883eb5dc4b8e6f8f
Aaron <aaronipollock@gmail.com>
2024-11-18 13:56:49 -0800

    csp

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const schema = require('./graphql/schema');
 const resolvers = require('./graphql/resolvers');
+const helmet = require('helmet');
 
 dotenv.config();
 const app = express();
@@ -17,5 +18,23 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true,
 }));
 
-const PORT = process.env.PORT || 5000;
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: ["'self'"],
+            styleSrc: ["'self'", 'https://fonts.googleapis.com'],
+            fontSrc: ["'self'", 'https://fonts.gstatic.com'],
+        },
+    })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Exapmle route
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
